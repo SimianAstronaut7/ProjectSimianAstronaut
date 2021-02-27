fbd1ed48930c7fb4515ddd83acb0b5bde7c75237
SimianAstronaut7 <79373020+SimianAstronaut7@users.noreply.github.com>
2021-02-27 07:54:54 -0800

    Update distribute_lib.ps1

--- Extensions/CommonLib/distribute_lib.ps1
+++ Extensions/CommonLib/distribute_lib.ps1
@@ -1,11 +1,14 @@
 # Make sure you allow unsigned PowerShell if using this.
-# $ Get-ExecutionPolicy
-# Needs to be "Unrestricted"
-# Run as admin
-# $ Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.1
+    # $ Get-ExecutionPolicy
+    # Needs to be "Unrestricted"
+    # Run as admin
+    # $ Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 
+# Expected to be run in the context of the directory the script is in.
 $Folders = Get-Item ..\* | Where-Object {$_.Name -ne "CommonLib"}
 
 foreach ($Folder in $Folders) {
     Copy-Item .\lib.js $Folder
+    Copy-Item .\jquery.min.js $Folder
 }
