fd455e8549bec0e3d54c3588facb294be9ce3943
SimianAstronaut7 <79373020+SimianAstronaut7@users.noreply.github.com>
2021-03-07 10:52:04 -0800

    Update Bloomberg_Banner_Removal.js
    Removes another annoyance

--- TamperMonkey/Bloomberg_Banner_Removal.js
+++ TamperMonkey/Bloomberg_Banner_Removal.js
@@ -2,18 +2,29 @@
 // @name         Bloomberg Banner Removal
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Remove an annoying banner
+// @description  Remove several annoying banners
 // @author       You
 // @match        https://www.bloomberg.com/news/articles/*
 // @grant        none
 // ==/UserScript==
-
+var top_banner=false;
+var overlay =false;
 function checkAndHide(){
     //console.log("*****RUNNING");
     var v = document.getElementById("banner-container");
+    var subscribe_overlay= document.querySelector("#newsletter-modal > div > div.header__400271f0 > div > button");
     //Banner may not have rendered yet. Retry in 2 seconds if not present.
     if(v){
         v.hidden=true;
+        top_banner = true
+    }
+    if (subscribe_overlay){
+        subscribe_overlay.click();
+        overlay=true;
+    }
+
+    if(top_banner && overlay){
+        return;
     }
     else{
         setTimeout(checkAndHide,2000);
